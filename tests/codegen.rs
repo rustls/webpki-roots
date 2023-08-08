0eb51ceeffaec5256b3ae3f5b54a656d5958be57
Daniel McCarney <daniel@binaryparadox.net>
2023-08-08 14:06:51 -0400

    codegen: remove unused ANSSI name constraints.
    The Agence Nationale de la Securite des Systemes d'Information (ANSSI)
    root is not present in the generated `lib.rs`, and so the associated
    name constraints are not used.

--- tests/codegen.rs
+++ tests/codegen.rs
@@ -96,7 +96,6 @@ async fn generated_code_is_fresh() {
     // https://hg.mozilla.org/projects/nss/file/tip/lib/certdb/genname.c such that
     // webpki-roots implements the same policy in this respect as the Mozilla root program.
     let mut imposed_constraints = HashMap::<Vec<u8>, Vec<u8>>::default();
-    imposed_constraints.insert(concat(ANSSI_SUBJECT_DN), concat(ANSSI_NAME_CONSTRAINTS));
     imposed_constraints.insert(
         concat(TUBITAK1_SUBJECT_DN),
         concat(TUBITAK1_NAME_CONSTRAINTS),
@@ -184,56 +183,6 @@ fn concat(parts: &[&[u8]]) -> Vec<u8> {
     }
     v
 }
-
-// Agence Nationale de la Securite des Systemes d'Information (ANSSI)
-const ANSSI_SUBJECT_DN: &[&[u8]] = &[
-    b"\x31\x0B\x30\x09\x06\x03\x55\x04\x06\x13\x02",
-    b"FR",
-    b"\x31\x0F\x30\x0D\x06\x03\x55\x04\x08\x13\x06",
-    b"France",
-    b"\x31\x0E\x30\x0C\x06\x03\x55\x04\x07\x13\x05",
-    b"Paris",
-    b"\x31\x10\x30\x0E\x06\x03\x55\x04\x0A\x13\x07",
-    b"PM/SGDN",
-    b"\x31\x0E\x30\x0C\x06\x03\x55\x04\x0B\x13\x05",
-    b"DCSSI",
-    b"\x31\x0E\x30\x0C\x06\x03\x55\x04\x03\x13\x05",
-    b"IGC/A",
-    b"\x31\x23\x30\x21\x06\x09\x2A\x86\x48\x86\xF7\x0D\x01\x09\x01",
-    b"\x16\x14",
-    b"igca@sgdn.pm.gouv.fr",
-];
-
-const ANSSI_NAME_CONSTRAINTS: &[&[u8]] = &[
-    b"\xa0\x5f",
-    b"\x30\x5D\xA0\x5B",
-    b"\x30\x05\x82\x03",
-    b".fr",
-    b"\x30\x05\x82\x03",
-    b".gp",
-    b"\x30\x05\x82\x03",
-    b".gf",
-    b"\x30\x05\x82\x03",
-    b".mq",
-    b"\x30\x05\x82\x03",
-    b".re",
-    b"\x30\x05\x82\x03",
-    b".yt",
-    b"\x30\x05\x82\x03",
-    b".pm",
-    b"\x30\x05\x82\x03",
-    b".bl",
-    b"\x30\x05\x82\x03",
-    b".mf",
-    b"\x30\x05\x82\x03",
-    b".wf",
-    b"\x30\x05\x82\x03",
-    b".pf",
-    b"\x30\x05\x82\x03",
-    b".nc",
-    b"\x30\x05\x82\x03",
-    b".tf",
-];
 
 // TUBITAK Kamu SM SSL Kok Sertifikasi - Surum 1
 const TUBITAK1_SUBJECT_DN: &[&[u8]] = &[
